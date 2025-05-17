26514cb0ce470285ff0ccce17a47dcb5d3720f39
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-05-17 17:50:06 -0400

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,15 +1,14 @@
 #python 3.12
 def calculadora():
-    try:
-        # Entrada de datos
-        entrada = int(input("Coloca el primer número para la operación: "))
-        segunda_entrada = int(input("Coloca el segundo número para sumar"))
-    except ValueError:
-        print("Es inválido vuelve a intentarlo con números")
-    resultado = entrada + segunda_entrada
-    # Sumando las entradas de datos
-    print(f"{entrada} + {segunda_entrada} + = {resultado}")
-    return [entrada, segunda_entrada, resultado, print]
-calculadora()
-if __name__ == '__main__':
-    calculadora()
+    while True:
+        try:
+            # Entrada de datos
+            entrada = int(input("Coloca el primer número para la operación: "))
+            segunda_entrada = int(input("Coloca el segundo número para sumar"))
+        except ValueError:
+            print("¡Es Inválido! vuelve a intentarlo con números")
+        resultado = entrada + segunda_entrada
+        # Sumando las entradas de datos
+        print(f"{entrada} + {segunda_entrada} = {resultado}")
+    if __name__ == '__main__':
+        calculadora()
