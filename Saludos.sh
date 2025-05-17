9da25c69214a37b595128735129b7b4d6719cff0
Thehacker <naelalejandrorengifocontreras@gmail.com>
2025-05-16 23:13:37 -0400

    Create Saludos.sh

---
+++ Saludos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Título
+printf -e "\e[31m#--------------------------#"
+printf -e "\e[32m#        Calculadora       #"
+printf -e "\e[33m#--------------------------#"
+# Salto de línea y restablecer colores
+echo -e "\e[0m"
+# Entrada de datos
+read -p "Quiéres Ejecutar directamente Calculadora.py :)" respuesta
+# Comprobando la respuesta del usuario
+if [[ "$respuesta" == "si" ]]; then
+   echo -e "\e[32m Ejecutando Calculadora.py ^_^"
+     sleep 2
+   exit 1
+   apt install python3
+   python3 Calculadora.py
+else
+    echo -e "\e[34 Saliendo Operación Terminada"
+fi
